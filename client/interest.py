e08ea2b86a25962551bc40d92a59fac8d110d4b0
JamesDeAntonis <jadeantonis@gmail.com>
2020-03-23 22:36:19 -0400

    fixed interest.py

--- client/interest.py
+++ client/interest.py
@@ -17,6 +17,7 @@ from client_obj import client as client
 # FOR GETTING THE PYTRENDS
 
 def get_interest_over_time(kw_list):
+	
 	us_states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 	geo_list = ['US-' + state for state in us_states]
 	geo_list.append('US')
