b6ea60fdaae3361fa5374b144b1289d61fc9064f
JamesDeAntonis <jadeantonis@gmail.com>
2020-03-23 22:08:13 -0400

    fixed the get_interest method

--- client/interest.py
+++ client/interest.py
@@ -17,20 +17,25 @@ from client_obj import client as client
 # FOR GETTING THE PYTRENDS
 
 def get_interest_over_time(kw_list):
-  geo_list = ['US-' + state for state in us_states]
-  geo_list.append('US')
-  first = True
-  for geo in geo_list:  # sometimes takes like 30s using all 50 states
-    pytrends.build_payload(kw_list, cat=0, 
-                          timeframe='2000-12-14 2017-01-25',
-                          geo=geo, gprop='')
-    temp = pytrends.interest_over_time().assign(state = geo[-2:])
-    if first == False:  # not the first iteration so join the two dfs
-      interest = interest.append(temp)
-    if first == True:  # on first iteration we initialize interest
-      interest = temp
-      first = False
-  return interest
+	us_states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
+    	         'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 
+        	     'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 
+            	 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 
+            	 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
+	geo_list = ['US-' + state for state in us_states]
+	geo_list.append('US')
+	first = True
+	for geo in geo_list:  # sometimes takes like 30s using all 50 states
+    	pytrends.build_payload(kw_list, cat=0, 
+        	                  timeframe='2000-12-14 2017-01-25',
+            	              geo=geo, gprop='')
+	    temp = pytrends.interest_over_time().assign(state = geo[-2:])
+    	if first == False:  # not the first iteration so join the two dfs
+      		interest = interest.append(temp)
+    	if first == True:  # on first iteration we initialize interest
+      		interest = temp
+      		first = False
+	return interest
 
 ##################################
 # MAIN
@@ -38,14 +43,8 @@ def get_interest_over_time(kw_list):
 def get_interest(entity):
 
 	pytrends = TrendReq(hl='en-US', tz=360)
-
-	us_states = ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-    	         'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD', 
-        	     'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 
-            	 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 
-            	 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY']
 	
 	interest = get_interest_over_time([entity]) \
     	          .rename(columns={entity: 'Interest'})
-    
-	client = client(entity, interest)
+	
+	return interest
