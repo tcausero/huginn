3f13a091dd2d2f816be7783790b2f2b553baad27
Jesse <jcahill225@gmail.com>
2020-03-29 12:50:24 -0400

    added exceptions.py

---
+++ client/exceptions.py
@@ -0,0 +1,13 @@
+class DotEnvError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
+
+class NytApiError(Exception):
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
