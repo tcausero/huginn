3d1769fbc629cdac62aa22c5aa44212d15102ce2
Jesse <jcahill225@gmail.com>
2020-03-30 21:22:21 -0400

    Added link finding to the client demo

--- client_demo.py
+++ client_demo.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append('client')
-
 from client.interest import get_interest
 from client.client_obj import client
 import client.getting_anomalies as anom
@@ -14,6 +11,13 @@ try:
 except:
 	print('could not find this entity in cache.  requesting pytrends...')
 	interest = get_interest(entity)
-cl = client(entity, interest)
 
+
+cl = client(entity, interest)
+print('Finding Anomalies')
 print(cl.get_anomalies(method=anom.version_3, lookback=10))
+print('Fetching links')
+print(cl.get_nyt_links())
+# print('Scraping content from links')
+# print('Summarizing content')
+
