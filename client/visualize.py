8a928b83f706950c808002a07e31f5f2e37c5c7c
JamesDeAntonis <jadeantonis@gmail.com>
2020-04-16 14:56:00 -0400

    added a urls attribute

--- client/visualize.py
+++ client/visualize.py
@@ -3,6 +3,23 @@
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import plotly
+import plotly.express as px
+
+def plot_data_plotly(data):
+    fig = px.line(data, x=data.index, y=data.columns[0])
+
+    fig.update_layout(
+        title='Google Search Interest in '+ data.columns[0] + ' over Time',
+        xaxis_title="Year",
+        yaxis_title="Interest (Standardized)",
+        font=dict(
+            family="Courier New, monospace",
+            size=12,
+            color="#7f7f7f"
+            )
+        )
+    return plotly.offline.plot(fig, auto_open=False, output_type='div')
 
 def plot_data(data):
     """ Plot the data (time series)
@@ -18,6 +35,58 @@ def plot_data(data):
     fig.autofmt_xdate()
     plt.legend()
     plt.show()
+
+def plot_data_with_anomalies_plotly(data, anomalies, as_var=False):
+    fig = px.line(data, x=data.index, y=data.columns[0])
+
+    shapes = []
+    for anomaly in anomalies:
+        # shapes.append({  # Unbounded line at x = 4
+        #         'type': 'line',
+        #         # x-reference is assigned to the x-values
+        #         'xref': 'x',
+        #         # y-reference is assigned to the plot paper [0,1]
+        #         'yref': 'paper',
+        #         'x0': anomaly,
+        #         'y0': 0,
+        #         'x1': anomaly,
+        #         'y1': 1,
+        #         'line': {
+        #             'color': 'rgb(55, 128, 191)',
+        #             'width': 3,
+        #         },
+        #     }
+        # )
+        shapes.append({# Unbounded span at 6 <= x <= 8
+                'type': 'rect',
+                # x-reference is assigned to the x-values
+                'xref': 'x',
+                # y-reference is assigned to the plot paper [0,1]
+                'yref': 'paper',
+                'x0': anomaly - pd.DateOffset(15),
+                'y0': 0,
+                'x1': anomaly + pd.DateOffset(15),
+                'y1': 1,
+                'fillcolor': 'red',
+                'opacity': 0.2,
+                'line': {
+                    'width': 0,
+                }
+            }
+        )
+
+    fig.update_layout(
+        title='Google Search Interest in '+ data.columns[0] + ' over Time',
+        xaxis_title="Year",
+        yaxis_title="Interest (Standardized)",
+        font=dict(
+            family="Courier New, monospace",
+            size=12,
+            color="#7f7f7f"
+            ),
+        shapes=shapes
+        )
+    return plotly.offline.plot(fig, auto_open=False, output_type='div')
 
 def plot_data_with_anomalies(data, anomalies, as_var=False):
     """ Plot the data and add anomalies as line on the graph
