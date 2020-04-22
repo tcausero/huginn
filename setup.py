d42b57b934abc252496f1b04a4932b6e84389a18
Jesse <jcahill225@gmail.com>
2020-04-22 18:20:37 -0400

    added readme to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='huginn',
       version='0.1',
       description='Know Your Client Tools From News Sources',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='http://github.com/tcausero/huginn',
       author='Jesse Cahill, Thomas Causero, James DeAntonis, Ryan McNally',
       author_email='jcahill225@gmail.com, tc3030@columbia.edu, jad2295@columbia.edu, rom2109@columbia.edu',
