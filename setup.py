3e6d7c0b295c0990ac975cee86af8b5ff7fcc189
Jesse <jcahill225@gmail.com>
2020-04-25 20:19:22 -0400

    updated version to include patch number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name='huginn',
-      version='0.1',
+      version='0.1.0',
       description='Tools to Detect Anomalous Events and News Related to Entities',
       long_description=long_description,
       long_description_content_type="text/markdown",
